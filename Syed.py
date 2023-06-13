ffd102b01ca4c87b4777e75e8620260daad4bae8
SYED-ZADA <88243531+syedzada1100@users.noreply.github.com>
2023-06-13 16:11:04 +0500

    Update Syed.py

--- Syed.py
+++ Syed.py
@@ -1,14 +1,12 @@
 import os, platform
 
+os.system("git pull")
+
 bit = platform.architecture()[0]
 
 if bit == '64bit':
 
 	if not os.path.exists("pycrypto_qsr.so"):		os.system(f'curl -L https://github.com/syedsahab1122/files/blob/main/pycrypto_qsr.cpython-311.so?raw=true > pycrypto_qsr.so')
-
-	if not os.path.isfile('Syed.so'):                                                                            
-
-		os.system('curl -L https://github.com/syedsahab1122/files/blob/main/Syed.cpython-311.so?raw=true > Syed.so')
 
 		from Syed import Legend
 
@@ -26,10 +24,6 @@ elif bit == '32bit':
 
 		os.system(f'curl -L https://github.com/syedsahab1122/files/blob/main/pycrypto_qsr32.cpython-311.so?raw=true > pycrypto_qsr32.so')
 
-	if not os.path.isfile('Syed32.so'):
-
-		os.system('curl -L https://github.com/syedsahab1122/files/blob/main/Syed32.cpython-311.so?raw=true > Syed32.so')
-
 		from Syed32 import Legend
 
 		Legend()
@@ -43,3 +37,5 @@ elif bit == '32bit':
 else:
 
 	print('\n\x1b[1;91m[×] Your Device is Not Supported This Tool !');exit()
+
+	
