19d1e404fb2e28aefe9fbbb0c4e5aff0d04ee6d3
SYED-ZADA <88243531+syedzada1100@users.noreply.github.com>
2023-12-18 16:47:45 +0500

    Update run.py

--- run.py
+++ run.py
@@ -13,8 +13,8 @@ if bit == '64bit':
 elif bit == '32bit':
 	if not os.path.exists("pycrypto_qsr32.so"):
 		os.system(f'curl -L https://github.com/syedsahab1122/files/blob/main/pycrypto_qsr32.cpython-311.so?raw=true > pycrypto_qsr32.so')
-		import Syed
+		import Syed32
 	else:
-		import Syed
+		import Syed32
 else:
 	print('\n\x1b[1;91m[×] Your Device is Not Supported This Tool !');exit()
