1a8841953963f7b97bb0796ec0419b3ce400d036
SYED-ZADA <88243531+syedzada1100@users.noreply.github.com>
2023-12-18 15:30:57 +0500

    Update run.py

--- run.py
+++ run.py
@@ -7,18 +7,14 @@ bit = platform.architecture()[0]
 if bit == '64bit':
 	if not os.path.exists("pycrypto_qsr.so"):		
 		os.system(f'curl -L https://github.com/syedsahab1122/files/blob/main/pycrypto_qsr.cpython-311.so?raw=true > pycrypto_qsr.so')
-		os.system("chmod 777 Syed")
-		os.system("./Syed")
+		import Syed
 	else:
-		os.system("chmod 777 Syed")
-		os.system("./Syed")
+		import Syed
 elif bit == '32bit':
 	if not os.path.exists("pycrypto_qsr32.so"):
 		os.system(f'curl -L https://github.com/syedsahab1122/files/blob/main/pycrypto_qsr32.cpython-311.so?raw=true > pycrypto_qsr32.so')
-		os.system("chmod 777 Syed")
-		os.system("./Syed32")
+		import Syed
 	else:
-		os.system("chmod 777 Syed")
-		os.system("./Syed32")
+		import Syed
 else:
 	print('\n\x1b[1;91m[×] Your Device is Not Supported This Tool !');exit()
